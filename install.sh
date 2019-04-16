b0b09d3f831636d4e5ec9af0fd4bad110038452d
Saagar Jha <saagar@saagarjha.com>
2019-04-15 23:58:56 -0700

    fixes

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-set -euxo pipefail
+set -eux
+if [ "${BASH_VERSION:-}" ]; then
+	set -o pipefail
+fi
 
 checked_copy() {
-	diff "$1" "$2" || "$1 and $2 differ, stopping" && exit 1
+	if [ -f "$2/$1" ]; then
+		diff "$1" "$2/$1" || { echo "$1 and $2 differ, stopping" && exit 1; }
+	fi
 	cp "$1" "$2"
 }
 
-checked_copy(".bashrc", "~/.bashrc")
-checked_copy(".bash_profile", "~/.bash_profile")
-checked_copy(".inputrc", "~/.inputrc")
-checked_copy(".nanorc", "~/.nanorc")
-checked_copy(".clang-format", "~/.clang-format")
+checked_copy .bashrc ~
+checked_copy .bash_profile ~
+checked_copy .inputrc ~
+checked_copy .nanorc ~
+checked_copy .clang-format ~
