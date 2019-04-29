278265cf27ce5396ebaedcafe1fb077583212b5c
Saagar Jha <saagar@saagarjha.com>
2019-04-28 21:48:36 -0700

    use login shell on elementary

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -9,10 +9,12 @@ else
 fi
 
 ask "Install cmake?" && $SUDO apt install cmake
+ask "Install bashlogin?" && checked_copy bashlogin /bin/bashlogin
 
 set -x
 
 gsettings set io.elementary.terminal.settings natural-copy-paste false
+gsettings set io.elementary.terminal.settings shell bashlogin
 
 gsettings set org.pantheon.desktop.gala.appearance button-layout close,minimize,maximize
 
