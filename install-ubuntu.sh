fe91335ecbe772a388c549a35bbdcb224b8824ad
Saagar Jha <saagar@saagarjha.com>
2019-04-28 21:48:03 -0700

    add some elementary settings

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -9,4 +9,13 @@ else
 fi
 
 ask "Install cmake?" && $SUDO apt install cmake
+
+set -x
+
+gsettings set io.elementary.terminal.settings natural-copy-paste false
+
+gsettings set org.pantheon.desktop.gala.appearance button-layout close,minimize,maximize
+
+{ set +x; } 2>/dev/null
+
 true
