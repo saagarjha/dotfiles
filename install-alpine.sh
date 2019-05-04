e0b8c5ea036e3bfc20c50be93edc2065439f965f
Saagar Jha <saagar@saagarjha.com>
2019-05-04 05:59:41 -0700

    add nano syntax

--- install-alpine.sh
+++ install-alpine.sh
@@ -4,7 +4,7 @@
 
 ask "Install bash?" && { apk add bash; ask "Change shell (bash is at $(which bash))?" && nano /etc/passwd; }
 ask "Install bash-completion?" && apk add bash-completion
-ask "Install nano?" && apk add nano
+ask "Install nano?" && apk add nano nano-syntax
 ask "Install gcc?" && apk add gcc libc-dev
 ask "Install g++?" && apk add g++
 ask "Install make?" && apk add make
