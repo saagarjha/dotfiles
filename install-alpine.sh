262aa3de0fd8cebd40c33f62ee5f3abbc1ef5979
Saagar Jha <saagar@saagarjha.com>
2019-05-25 23:41:06 -0700

    install less on Alpine

--- install-alpine.sh
+++ install-alpine.sh
@@ -2,9 +2,12 @@
 
 . ./shared.sh
 
+cat /dev/null > /etc/motd
+
 ask "Install bash?" && { apk add bash; ask "Change shell (bash is at $(which bash))?" && nano /etc/passwd; }
 ask "Install bash-completion?" && apk add bash-completion
 ask "Install nano?" && apk add nano nano-syntax
+ask "Install less?" && apk add less
 ask "Install gcc?" && apk add gcc libc-dev
 ask "Install g++?" && apk add g++
 ask "Install make?" && apk add make
