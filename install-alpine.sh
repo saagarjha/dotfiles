b8757817e8ec06756dfcdea36a762435ca165bea
Saagar Jha <saagar@saagarjha.com>
2019-12-25 19:09:12 -0500

    install man

--- install-alpine.sh
+++ install-alpine.sh
@@ -8,6 +8,7 @@ ask "Install bash?" && { apk add bash; echo "Make sure to change your shell (bas
 ask "Install bash-completion?" && apk add bash-completion
 ask "Install nano?" && apk add nano nano-syntax
 ask "Install less?" && apk add less
+ask "Install man?" && apk add man
 ask "Install build-base?" && apk add build-base
 ask "Install cmake?" && apk add cmake
 ask "Install ag?" && apk add the_silver_searcher
