b3e80492efd8acd4c007f6d7fdd0224358bce585
Saagar Jha <saagar@saagarjha.com>
2019-12-25 19:08:59 -0500

    replace miscellaneous build tools with build-base

--- install-alpine.sh
+++ install-alpine.sh
@@ -8,9 +8,7 @@ ask "Install bash?" && { apk add bash; echo "Make sure to change your shell (bas
 ask "Install bash-completion?" && apk add bash-completion
 ask "Install nano?" && apk add nano nano-syntax
 ask "Install less?" && apk add less
-ask "Install gcc?" && apk add gcc libc-dev
-ask "Install g++?" && apk add g++
-ask "Install make?" && apk add make
+ask "Install build-base?" && apk add build-base
 ask "Install cmake?" && apk add cmake
 ask "Install ag?" && apk add the_silver_searcher
 true
