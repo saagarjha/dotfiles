35b4497e45c8a53a31031c8af65a96c470cd0fff
Saagar Jha <saagar@saagarjha.com>
2020-06-10 16:10:58 -0700

    add a sysctl plist, currently enables AMFI logging

--- install-macos.sh
+++ install-macos.sh
@@ -139,4 +139,6 @@ ask "Install Xcode keybindings?" && checked_copy Default.idekeybindings ~/Librar
 ask "Install Xcode themes?" && checked_copy FontAndColorThemes ~/Library/Developer/Xcode/UserData/FontAndColorThemes && defaults write com.apple.dt.Xcode XCFontAndColorCurrentTheme -string 'Solarized (Light).xccolortheme'
 ask "Install iterm-theme.py?" && mkdir -p "~/Library/Application Support/iTerm2/Scripts/AutoLaunch/" && checked_copy iterm-theme.py "~/Library/Application Support/iTerm2/Scripts/AutoLaunch/iterm-theme.py"
 
+sudo cp sysctl.plist /Library/LaunchDaemons
+
 true
